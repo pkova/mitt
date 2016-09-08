180eca29fa0894366d935efda49b2fc01b91b12a
Pyry Kovanen <pyry.kovanen@gmail.com>
2016-09-07 23:45:25 -0700

    Refactor app-routes middleware

--- src/mitt/handler.clj
+++ src/mitt/handler.clj
@@ -1,7 +1,7 @@
 (ns mitt.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
             [clj-http.client :as http]
             [ring.util.response :as resp]))
 
@@ -11,6 +11,4 @@
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults
-   app-routes
-   (assoc-in site-defaults [:security :anti-forgery] false)))
+  (wrap-defaults app-routes api-defaults))
