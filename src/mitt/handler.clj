06c80ced3720f060f724b504fd1ec5fbbb46c2ff
Pyry Kovanen <pyry.kovanen@gmail.com>
2016-09-07 17:50:35 -0700

    GET to root now serves simple html file

--- src/mitt/handler.clj
+++ src/mitt/handler.clj
@@ -1,10 +1,12 @@
 (ns mitt.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [clj-http.client :as http]
+            [ring.util.response :as resp]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (resp/redirect "/index.html"))
   (route/not-found "Not Found"))
 
 (def app
