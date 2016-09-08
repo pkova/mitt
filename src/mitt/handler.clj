7c3f57879dde18020e3f682c2367892f56ad5d01
Pyry Kovanen <pyry.kovanen@gmail.com>
2016-09-08 00:16:28 -0700

    Add json parsing middleware

--- src/mitt/handler.clj
+++ src/mitt/handler.clj
@@ -2,13 +2,16 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
+            [ring.middleware.json :refer [wrap-json-body]]
             [clj-http.client :as http]
             [ring.util.response :as resp]))
 
 (defroutes app-routes
   (GET "/" [] (resp/redirect "/index.html"))
-  (POST "/prices" x (str x))
+  (POST "/prices" x (str (get-in x [:body "hello"])))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes api-defaults))
+  (-> app-routes
+      (wrap-json-body)
+      (wrap-defaults api-defaults)))
